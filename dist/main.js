0f7767545a394ce06038be29e830450de4241dd3
faris <faris38kanbur@gmail.com>
2021-04-09 11:56:58 +0200

    add dist folder

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("faris Kanbur"),console.log("ravalynm"),console.log("Hjulmar")})();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {wizard} from './wizard';
+import {barbarian} from './barbarian';
+
+console.log("faris Kanbur")
+console.log(wizard)
+console.log(barbarian)
